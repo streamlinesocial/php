79fba994e9612245b9592b9e9cb792d6ad73fbb5
Brandon Burton <brandon@inatree.org>
2012-11-19 22:47:30 -0800

    Update recipes/module_gd.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,13 +20,6 @@
 #
 
 include_recipe "php::#{node['php']['install_method']}"
-pkg = value_for_platform(
-  %w(centos redhat scientific fedora amazon) => {
-    %w(5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8) => "php53-mysql",
-    "default" => "php-mysql"
-  },
-  "default" => "php5-mysql"
-)
 
 # update the main channels
 php_pear_channel 'pear.php.net' do
